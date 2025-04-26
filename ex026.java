38dd82e4773dbb75228ee126d8a8f8d1ade69721
Ariel Marinho <167530405+arieldreis@users.noreply.github.com>
2025-04-26 20:48:50 -0300

    Novo exercícos
    EX026

---
+++ ex026.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ex026 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o seu Nome: ");
+        String nome = input.nextLine();
+        System.out.println("O seu nome é " + nome);
+    }
+}
