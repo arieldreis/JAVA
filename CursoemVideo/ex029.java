210cdbb8de9cbe31774c25ca81c50ef0fc0dee3f
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-07-05 17:07:21 -0300

    I fix some logic erros in my code

--- CursoemVideo/ex029.java
+++ CursoemVideo/ex029.java
@@ -3,26 +3,49 @@ public class ex029 {
     public static void main(String[] args) {
         String nome = JOptionPane.showInputDialog(null, "<html>Digite o seu <b>Nome:</b></html>");
         int soma = 0;
-        int quant = 0;
-        while(true){
-            int numeros = Integer.parseInt(JOptionPane.showInputDialog(null, "<html>Digite um <b>número</b>:</html>"));
+        int totalValues = 0;
+        int pares = 0;
+        int impares = 0;
+        int acimaCem = 0;
+        double mediaValues = 0;
+        int numeros;
+        do{
+            numeros = Integer.parseInt(
+                    JOptionPane.showInputDialog(null,
+                            "<html>Digite um <b>número</b>:<br><em>(0 interrompe)</em></html>"));
+                if (numeros != 0){
+                    totalValues++;
+                    if (numeros % 2 == 0){
+                        pares++;
+                    }
+                    if (numeros % 2 == 1){
+                        impares++;
+                    }
+                    if(numeros > 100){
+                        acimaCem++;
+                    }
+                }
             soma+=numeros;
-            if(numeros == JOptionPane.CANCEL_OPTION){
-                JOptionPane.showMessageDialog(null,
+        }while (numeros != 0);
+        mediaValues = soma / totalValues;
+        JOptionPane.showMessageDialog(null,
                 "<html>" +
-                "Olá" +
-                nome +
-                "olha o resultado das operações abaixo.<br>" +
-                "Total de Valores: " +
-                "Total de pares: " + 
-                "Total de ímpares:" + 
-                "Acima de 100:" + 
-                "Média dos Valores: " +
-                "</html>");
-                break;
-            }else{
-                JOptionPane.showMessageDialog(null, "<html>Valor cadstrado com sucesso.</html>");
-            }
-        }
+                        "<h3>Olá " +
+                        nome +
+                        "!</h3><br>" +
+                        "Veja o resultado das operações abaixo.<br>" +
+                        "Soma dos Valores: " +
+                        soma +
+                        "<br>Total de Valores: " +
+                        totalValues +
+                        "<br>Total de pares: " +
+                        pares +
+                        "<br>Total de ímpares: " +
+                        impares +
+                        "<br>Acima de 100: " +
+                        acimaCem +
+                        "<br>Média dos Valores: " +
+                        mediaValues +
+                        "</html>");
     }
 }
