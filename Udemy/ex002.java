cf6adc7e89d8b5634aaf3ca83c7889ca14204553
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-07-09 13:04:19 -0300

    Ternary operator in JAVA

---
+++ Udemy/ex002.java
@@ -0,0 +1,7 @@
+public class ex002 {
+    public static void main(String[] args) {
+       double preco = 34.5;
+       double desconto = (preco < 20.0) ? preco* 0.1 : preco * 0.05;
+       System.out.println(desconto);
+    }
+}
