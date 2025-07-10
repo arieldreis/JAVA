6e5705272b052bbedde51b4b9cda4e75c09c3595
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-07-09 22:12:50 -0300

    How to execute a function in Java

---
+++ Udemy/ex004.java
@@ -0,0 +1,26 @@
+public class ex004 {
+    public static void main(String[] args) {
+        String txt = "I love too much Java!";
+        System.out.println(txt.toLowerCase());
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.trim());
+        System.out.println(txt.substring(3, 12));
+        System.out.println(txt.substring(4));
+        System.out.println(txt.replace("Java", "Python"));
+        System.out.println("A palavra 'much' começa no índice " + txt.indexOf("much"));
+        System.out.println("A palavra 'much' começa no índice " + txt.indexOf("planet")); // Estará no indice negativo pq essa palavra não existe nessa variável.
+        System.out.println(txt.lastIndexOf("too"));
+        // como fatiar uma string
+        String[] vect  = txt.split(" ");
+        String word1 = vect[0];
+        System.out.println(word1);
+        String word2 = vect[1];
+        System.out.println(word2);
+        String word3 = vect[2];
+        System.out.println(word3);
+        String word4 = vect[3];
+        System.out.println(word4);
+        String word5 = vect[4];
+        System.out.println(word5);
+    }
+}
---
+++ Udemy/ex005.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ex005 {
+    public static void main(String[] args) {
+        double x = Math.sqrt(64);
+        System.out.println(x);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int higher = max(a, b, c);
+        showResult(higher);
+        sc.close();
+    }
+    public static int max(int x, int y, int z){
+        int aux;
+        if(x > y && x > z){
+            aux = x;
+        } else if (y > x && y > z) {
+            aux = y;
+        }else{
+            aux = z;
+        }
+        return aux;
+    }
+    public static void showResult(int value){
+        System.out.println("Higher = " + value);
+    }
+}
