9010093efd8f03f0f638cbb83c37c4c8313522e2
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-12-16 17:12:04 -0300

    Solving ex003

---
+++ Udemy/arrays/array05.java
@@ -0,0 +1,29 @@
+package arrays;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class array05 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar?");
+        int n = sc.nextInt();
+        int[] numeros = new int[n];
+        int QuantPar = 0;
+        for(int index = 0; index < n; index++){
+            System.out.println("Digite um numero:");
+            numeros[index] = sc.nextInt();
+        }
+        System.out.println("Números Pares: ");
+        for(int index = 0; index < n; index++){
+            if(numeros[index] % 2 == 0){
+                System.out.print(numeros[index] + " ");
+                QuantPar++;
+            }
+        }
+        System.out.println("\nQuantidade de Números Pares: " + QuantPar);
+    }
+}
