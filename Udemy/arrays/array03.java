d1d6da77054253add48048dc44454d373883d958
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-12-16 15:28:45 -0300

    solving ex001

---
+++ Udemy/arrays/array03.java
@@ -0,0 +1,31 @@
+package arrays;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Locale;
+
+
+public class array03 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantos numeros voce vai digitar?");
+        int n = sc.nextInt();
+        double[] numbers = new double[n];
+        int negativo = 0;
+        for(int index = 0; index < n; index++){
+            System.out.println("Digite um número: ");
+            numbers[index] = sc.nextDouble();
+        }
+        System.out.println(Arrays.toString(numbers)); // This line code will display the elements inside of array.
+        System.out.println("Negative Numbers: ");
+        for(double number : numbers){
+            if(number < 0){
+                System.out.println(number);
+            }else if(number > 0){
+                System.out.println("There isn't any negative number.");
+            }
+        }
+
+        sc.close();
+    }
+}
