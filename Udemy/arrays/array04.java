8ab2e12de4e08c77cf042c1782b59d3a5351510e
Ariel dos Reis Marinho <arieldreis@gmail.com>
2025-12-16 15:59:00 -0300

    Solving ex002

---
+++ Udemy/arrays/array04.java
@@ -0,0 +1,24 @@
+package arrays;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class array04 {
+    public static void main(String[] args){
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos numeros voce vai digitar?");
+        int repeticao = sc.nextInt();
+        double[] numeros = new double[repeticao];
+
+        for(int index = 0; index < repeticao; index++){
+            System.out.println("Digite um numero:");
+            numeros[index] = sc.nextDouble();
+        }
+        System.out.println();
+        System.out.printf("Valores = %s\n", Arrays.toString(numeros));
+        System.out.printf("Soma = %.2f\n", Arrays.stream(numeros).sum());
+        OptionalDouble media = Arrays.stream(numeros).average();
+        System.out.println("Media = " + media);
+    }
+}
