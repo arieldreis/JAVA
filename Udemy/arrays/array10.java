04efa7fb49a34a155941505b91eeb89b36adc7d1
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-05 15:11:03 -0300

    Array 10 fixing issues

--- Udemy/arrays/array10.java
+++ Udemy/arrays/array10.java
@@ -7,14 +7,44 @@ public class array10 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Quantas pessoas serao digitadas? ");
         int n = sc.nextInt();
-        String nome;
-        double[] vetor = new double[n]; // O valor n que está dentro da array representa o tamanho dela.
-        String[] vetorSexo = new String[nome];
-        for (int index = 1; index < n; index++) {
-            System.out.printf("Altura %d° pessoa: ", index);
-            vetor[index] = sc.nextDouble(); // Pegue o número que o usuário digitou e guarde na posição index da array.
-            System.out.printf("Genero %d° pessoa: ", index);
+        sc.nextLine();
+
+        double[] altura = new double[n]; // O valor n que está dentro da array representa o tamanho dela.
+        char[] sexo = new char[n];
+
+        double maiorAltura = 0.0;
+        double menorAltura = 0.0;
+        for (int index = 0; index < n; index++) {
+            System.out.printf("Altura %d° pessoa: ", index + 1);
+            altura[index] = sc.nextDouble(); // Pegue o número que o usuário digitou e guarde na posição index da array.
+            System.out.printf("Genero %d° pessoa: ", index + 1);
+            sexo[index] = sc.next().charAt(0);
+            sc.nextLine();
+
+            if(altura[index] > maiorAltura){
+                maiorAltura = altura[index];
+            }
+            if(altura[index] < menorAltura){
+                menorAltura = altura[index];
+            }
         }
+        int totH = 0;
+        int totM = 0;
+        double somaMulheres = 0.0;
+
+        for(int index= 0; index < n; index++){
+            if(sexo[index] == 'M'){
+                totH++;
+            }else{
+                somaMulheres += altura[index];
+                totM++;
+            }
+        }
+        double media = somaMulheres / totM;
+        System.out.printf("Menor altura = %.2f", menorAltura);
+        System.out.printf("\nMaior altura = %.2f", maiorAltura);
+        System.out.printf("\nMédia das alturas das mulheres = %.2f", media);
+        System.out.printf("\nNúmeros de homens = %d", totH);
         sc.close();
     }
 }
