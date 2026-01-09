4949c18a713e6dead4924f439fe98b83aa63336b
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-09 18:53:16 -0300

    Ajusting errors

--- Udemy/arrays/entities/funcionario.java
+++ Udemy/arrays/entities/funcionario.java
@@ -3,32 +3,35 @@ package arrays.entities;
 import java.util.List;
 
 public class funcionario {
-    private int id;
-    private String name;
-    private double salary;
+    private Integer id;
+    String name;
+    private Double salary;
 
-    public void increaseSalary(int id, String name, double salary){
+    public funcionario(Integer id, String name, Double salary) {
+    }
+
+    public void increaseSalary(Integer id, String name, Double salary){
         this.id = id;
         this.name = name;
         this.salary = salary;
     }
 
-    public int getId(List<Integer> idEmployee){
+    public int getId(){
         return id;
     }
-    public void setId(int id){
+    public void setId(Integer id){
         this.id = id;
     }
-    public String getName(List<String> nameEmployee){
+    public String getName(){
         return name;
     }
     public void setName(String name) {
         this.name = name;
     }
-    public Double getSalary(List<Double> salaryEmployee){
+    public Double getSalary(){
         return salary;
     }
-    public void setSalary(double salary){
+    public void setSalary(Double salary){
         this.salary = salary;
     }
 
--- Udemy/arrays/exercises/EmpresaFuncionarios.java
+++ Udemy/arrays/exercises/EmpresaFuncionarios.java
@@ -1,26 +1,14 @@
 package arrays.exercises;
 import java.util.*;
-
 import arrays.entities.funcionario;
 import java.util.ArrayList;
+
 public class EmpresaFuncionarios {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        funcionario empresa = new funcionario();
-
-        List<Integer> idEmployee = new ArrayList<Integer>();
-        List<String> nameEmployee = new ArrayList<String>();
-        List<Double> salaryEmployee = new ArrayList<Double>();
-
-
-        List<Object> dadosGerais = new ArrayList<>();
-
-        int id;
-        String name;
-        double salary = 0.0;
-
+        List<funcionario> funcionarios = new ArrayList<>();
 
         System.out.println("How many employees will be registered?");
         int qtd = sc.nextInt();
@@ -29,39 +17,29 @@ public class EmpresaFuncionarios {
         for(int index = 0; index < qtd; index++){
             System.out.printf("Employee #%d\n", index + 1);
             System.out.println("ID: ");
-            id = sc.nextInt();
+            Integer id = sc.nextInt();
             sc.nextLine();
             System.out.println("Name: ");
-            name = sc.nextLine();
+            String name = sc.nextLine();
             System.out.println("Salary: ");
-            salary = sc.nextDouble();
+            Double salary = sc.nextDouble();
+
+            funcionario employee = new funcionario(id, name, salary);
 
             sc.nextLine();
-
-            // Adicionando os elementos a lista
-            idEmployee.add(id);
-            nameEmployee.add(name);
-            salaryEmployee.add(salary);
-
-            dadosGerais.add(idEmployee);
-            dadosGerais.add(nameEmployee);
-            dadosGerais.add(salaryEmployee);
-
-            // Adicionado ás variavéis ao objeto empresa
-            empresa.getId(Collections.singletonList(id));
-            empresa.getName(Collections.singletonList(name));
-            empresa.getSalary(Collections.singletonList(salary));
+            funcionarios.add(employee);
         }
-        System.out.println("----------------------------");
-        System.out.println("List of Employee: ");
-        for(int index = 0; index < dadosGerais.toArray().length; index++){
-            System.out.println("ID: " + dadosGerais.get(index));
-            System.out.println("Nome: " + dadosGerais.get(index));
-            System.out.println("Salário: " + dadosGerais.get(index));
-            break;
-        }
-        System.out.println("----------------------------");
-        empresa.show();
+        System.out.println("Enter the employee id that will have salary increase: ");
+        int idSalario = sc.nextInt();
         sc.close();
+    }
+
+    public static Integer position(List<funcionario> funcionarios, int id) {
+        for (int index = 0; index < funcionarios.size(); index++) {
+            if (funcionarios.get(index).getId() == id) {
+                return index;
+            }
+        }
+        return null;
     }
 }
