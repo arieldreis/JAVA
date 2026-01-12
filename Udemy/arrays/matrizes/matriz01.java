24966ba4e945c359a172d264a20f531ab6fdca0c
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-12 17:07:19 -0300

    exercise of matriz

--- Udemy/arrays/matrizes/matriz01.java
+++ Udemy/arrays/matrizes/matriz01.java
@@ -1,7 +1,28 @@
 package arrays.matrizes;
-
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Scanner;
 public class matriz01 {
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] [] mat = new int[n][n];
+        int NegativeNumbers = 0;
+
+        for (int index = 0; index < mat.length; index++) {
+            for (int j = 0; j < mat[index].length; j++) {
+                mat[index][j] = sc.nextInt();
+                if (mat[index][j] < 0){
+                    NegativeNumbers++;
+                }
+            }
+        }
+        System.out.println("Main diagonal:");
+        for (int index = 0; index < mat.length; index++) {
+            System.out.println(mat[index][index] + " ");
+        }
+        System.out.println("Negative Numbers: " + NegativeNumbers);
     }
 }
