7f2ee29f31b8889e1c4063a47ac8a3f5ab504701
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-01-31 00:22:47 -0300

    Date and Time in Java

---
+++ Udemy/data_hora/date01.java
@@ -0,0 +1,20 @@
+package data_hora;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class date01 {
+    public static void main(String[] args) {
+        LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now(); // Horário global 'Londres'
+        LocalDate  d04 = LocalDate.parse("2026-01-31");
+        LocalDateTime d05 = LocalDateTime.parse("2026-01-31T00:20:26");
+
+        System.out.println("d01 = " + d01);
+        System.out.println("d02 = " + d02);
+        System.out.println("d03 = " + d03);
+        System.out.println("d04 = " + d04);
+        System.out.println("d05 = " + d05);
+    }
+}
--- Udemy/validacao/validarEmail.java
+++ Udemy/validacao/validarEmail.java
@@ -1,14 +1,14 @@
 package validacao;
 import java.util.Scanner;
-import org.apache.commons.lang3.StringUtils;
+// import org.apache.commons.lang3.StringUtils;
 public class validarEmail {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine().trim();
 
-        if(!StringUtils.isEmail(input)){
+        /*if(!StringUtils.isEmail(input)){
             throw new IllegalArgumentException("E-mail inválido");
-        }
+        }*/
 
         System.out.println("E-mail válido " + input);
     }
