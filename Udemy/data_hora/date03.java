853a072aade438b1d317f4f15d9025400197b5c5
Ariel dos Reis Marinho <arieldreis@gmail.com>
2026-03-03 15:39:48 -0300

    Date and calender

--- Udemy/data_hora/date03.java
+++ Udemy/data_hora/date03.java
@@ -3,6 +3,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 public class date03 {
     public static void main(String[] args) {
@@ -10,13 +11,33 @@ public class date03 {
         LocalDateTime d05 = LocalDateTime.parse("2026-02-21T01:30:26");
         Instant d06 = Instant.parse("2026-02-21T01:30:26Z");
 
-        LocalDateTime pastWeekLocalDateTime = d05.minusDays(13);
-        LocalDateTime nextWeekLocalDateTime = d05.plusDays(14);
+        LocalDate pastWeekLocalDate = d04.minusDays(7); // d04 - 7 days
+        LocalDate nextWeekLocalDate = d04.plusDays(7); // d04 + 7 days
 
-        Duration t1 =  Duration.between(pastWeekLocalDateTime.at, d04); // Calcula o tempo entre duas datas.
-        Duration t2 =  Duration.between(pastWeekLocalDateTime, d05); // Calcula o tempo entre duas datas.
+        System.out.println("pastWeekLocalDate = " + pastWeekLocalDate);
+        System.out.println("nextWeekLocalDate = " + nextWeekLocalDate);
 
-        System.out.println("t1 dias = " + t1.toMinutes()); // Transforma o tempo entre uma data e outra e converte para minutos
-        System.out.println("t2 dias = " + t2.toMinutes());
+        LocalDateTime pastWeekLocalDateTime = d05.plusHours(3).plusMinutes(15);
+        LocalDateTime nextWeekLocalDateTime = d05.plusSeconds(16);
+        System.out.println("--------------------------------");
+
+        System.out.println("pastWeekLocalDateTime = " + pastWeekLocalDateTime);
+        System.out.println("nextWeekLocalDateTime = " + nextWeekLocalDateTime);
+        System.out.println("--------------------------------");
+
+        Instant pastWeekInstant = d06.minus(7, ChronoUnit.HOURS);
+        Instant nextWeekInstant  = d06.plus(7, ChronoUnit.DAYS);
+
+        System.out.println("pastWeekInstant = " + pastWeekInstant);
+        System.out.println("nextWeekInstant = " + nextWeekInstant);
+
+        Duration t1 = Duration.between(pastWeekLocalDate.atStartOfDay(), d04.atStartOfDay()); // Covert LocalDate type to LocalDateTime
+        Duration t2 = Duration.between(pastWeekLocalDateTime, d05);
+        Duration t3 = Duration.between(pastWeekInstant, d06);
+        System.out.println("--------------------------------");
+
+        System.out.println("t1 dias = " + t1.toDays());
+        System.out.println("t2 horas = " + t2.toHours());
+        System.out.println("t3 dias = " + t3.toHours());
     }
 }
---
+++ Udemy/data_hora/date04.java
@@ -0,0 +1,17 @@
+package data_hora;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class date04 {
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Date y1 = sdf1.parse("03/03/2026");
+        Date y2 = sdf2.parse("03/03/2026 15:42:07");
+
+        System.out.println(y1);
+        System.out.println(y2);
+    }
+}
